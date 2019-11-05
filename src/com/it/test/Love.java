ba2aa617cfdb9f2b32ce179cdb40845342fa8bdd
lr_glb <871317045_glb@qq.com>
2019-11-05 22:20:47 +0800

    first iead commit

---
+++ src/com/it/test/Love.java
@@ -0,0 +1,8 @@
+package com.it.test;
+
+public class Love {
+    public static void main(String[] args) {
+        System.out.println("hello my love!");
+        System.out.println("hello myaaaaa love!");
+    }
+}
